2a81deaad7fbeec1073b14ea07869d24995c5071
Luiz Guilherme Rodrigues <91224521+guihard1@users.noreply.github.com>
2023-07-20 15:02:50 -0300

    Alguns exercicios de JavaScript
    exercicios de If, else, function, etc.

---
+++ Aula5(Condição)/ex001.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+if (vel <= 60.5) {
+    console.log(`A velocidade do seu carro é de ${vel}KM/h`)
+    console.log('Você está no limite de velocidade, dirija com segurança.');
+} else {
+    console.log(`A velocidade do seu carro é de ${vel}KM/h`)
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
---
+++ Aula5(Condição)/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo no ${pais}`);
+if (pais != 'Brasil') {
+    console.log('Você é estrangeiro');
+} else {
+    console.log('Você é Brasileiro');
+}
